172e4e1ae24cd63c8ed696170a11eae2d660a752
Paul Walker <legolovergo@icloud.com>
2022-03-15 17:06:49 -0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent extends BaseComponent {
   render() {
     return html`
       <app-header brandname="Placeholder" pages="Home,About"></app-header>
-      <h1>Hello, World!</h1>
+      <h1>Smash the JELLY!!!!</h1>
     `
   }
 }
